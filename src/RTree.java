fac70c141b30ad0a2f1cdac11042f90a157ee84d
karlitrotski <cpdettoni@gmail.com>
2014-09-19 17:41:13 -0300

    holito14 mejora quadratic

--- src/RTree.java
+++ src/RTree.java
@@ -160,105 +160,135 @@ public class RTree extends NodeSize{
 		}*/
 		return result;
 	}
-	public void QuadraticSplit(Rectangle r, Node node) throws IOException{
-		if(node.numRectangles >= 2*t){
-			int[] maxRs = this.maxIncrement(r, node);
-			Node son1 = new Node(nextDir,node.isLeaf);
-			nextDir += 2*B;
-			Node son2 = new Node(nextDir,node.isLeaf);
-			nextDir += 2*B;
-			/* Si el rectángulo a insertarse no es semilla, se mete al arreglo en lugar de la semilla. */
-			if(maxRs[0] != -1)
-				node.rectangles[maxRs[0]] = r;
-			/* Se guardan las semillas en los nodos nuevos. */
-			if(maxRs[0] == -1)
-				son1.putRectangle(r);
-			else
-				son1.putRectangle(node.rectangles[maxRs[0]]);
-			son2.putRectangle(node.rectangles[maxRs[1]]);
-			/* Se borra del arreglo el otro nodo semilla. */
-			node.rectangles[maxRs[1]] = null;
-			int bestDif = -1;
-			int bestInd = 0;
-			int g1 = 0;
-			int g2 = 0;
-			for(int i = 0; i < 2*t; i++){
-				if(son1.numRectangles == t+1){
-					for(int j = 0; j < 2*t; j++){
-						if(node.rectangles[j] != null)
-							//System.out.println("lastInsert "+i+": "+j);
-							son2.putRectangle(node.rectangles[j]);
-					}
-					break;
-				}
-				else if(son2.numRectangles == t+1){
-					for(int j = 0; j < 2*t; j++){
-						if(node.rectangles[j] != null)
-							//System.out.println("lastInsert "+i+": "+j);
-							son1.putRectangle(node.rectangles[j]);
-					}
-					break;
-				}
+	public Node QuadraticSplit(Rectangle r, Node node) throws IOException{
+		int[] maxRs = this.maxIncrement(r, node);
+		Node son1 = new Node(nextDir,node.isLeaf);
+		nextDir += 2*B;
+		Node son2 = new Node(nextDir,node.isLeaf);
+		nextDir += 2*B;
+		/* Si el rectángulo a insertarse no es semilla, se mete al arreglo en lugar de la semilla. */
+		if(maxRs[0] != -1)
+			node.rectangles[maxRs[0]] = r;
+		/* Se guardan las semillas en los nodos nuevos. */
+		if(maxRs[0] == -1)
+			son1.putRectangle(r);
+		else
+			son1.putRectangle(node.rectangles[maxRs[0]]);
+		son2.putRectangle(node.rectangles[maxRs[1]]);
+		/* Se borra del arreglo el otro nodo semilla. */
+		node.rectangles[maxRs[1]] = null;
+		int bestDif = -1;
+		int bestInd = 0;
+		int g1 = 0;
+		int g2 = 0;
+		for(int i = 0; i < 2*t; i++){
+			if(son1.numRectangles == t+1){
 				for(int j = 0; j < 2*t; j++){
-					if(node.rectangles[j] == null)
-						continue;
-					if(son1.numRectangles > 0)
-						g1 = son1.getMBR().getIncreasedArea(node.rectangles[j]) - son1.getMBR().getArea();
-					if(son2.numRectangles > 0)
-						g2 = son2.getMBR().getIncreasedArea(node.rectangles[j]) - son2.getMBR().getArea();
-					if(Math.abs(g1-g2) > bestDif){
-						bestDif = Math.abs(g1-g2);
-						bestInd = j;
+					if(node.rectangles[j] != null){
+						son2.sonsPos[son2.numRectangles] = node.sonsPos[j];
+						son2.putRectangle(node.rectangles[j]);
 					}
 				}
-				//System.out.println("bestInd "+i+": "+bestInd);
-				if(g1 < g2)
-					son1.putRectangle(node.rectangles[bestInd]);
-				else if(g2 < g1)
-					son2.putRectangle(node.rectangles[bestInd]);
-				else if(son1.getMBR().getArea() < son2.getMBR().getArea())
-					son1.putRectangle(node.rectangles[bestInd]);
-				else if(son1.getMBR().getArea() < son2.getMBR().getArea())
-					son2.putRectangle(node.rectangles[bestInd]);
-				else if(son1.numRectangles < son2.numRectangles)
-					son1.putRectangle(node.rectangles[bestInd]);
-				else
-					son2.putRectangle(node.rectangles[bestInd]);
-				node.rectangles[bestInd] = null;
-				bestDif = -1;
+				break;
 			}
-			if(node.fatherPos == -1){ // si es la raiz
-				Node newRoot = new Node(node.pos,0);
-				newRoot.fatherPos = node.fatherPos;
-				newRoot.putRectangle(son1.getMBR());
-				newRoot.putRectangle(son2.getMBR());
-				son1.fatherPos = newRoot.pos;
-				son2.fatherPos = newRoot.pos;
-				newRoot.isLeaf = 0;
-				newRoot.sonsPos[0] = son1.pos;
-				newRoot.sonsPos[1] = son2.pos;
-				this.nodeToExt(newRoot);
-				this.nodeToExt(son1); //guardar nodos
-				this.nodeToExt(son2);
+			else if(son2.numRectangles == t+1){
+				for(int j = 0; j < 2*t; j++){
+					if(node.rectangles[j] != null){
+						son1.sonsPos[son1.numRectangles] = node.sonsPos[j];
+						son1.putRectangle(node.rectangles[j]);
+					}
+				}
+				break;
+			}
+			for(int j = 0; j < 2*t; j++){
+				if(node.rectangles[j] == null)
+					continue;
+				if(son1.numRectangles > 0)
+					g1 = son1.getMBR().getIncreasedArea(node.rectangles[j]) - son1.getMBR().getArea();
+				if(son2.numRectangles > 0)
+					g2 = son2.getMBR().getIncreasedArea(node.rectangles[j]) - son2.getMBR().getArea();
+				if(Math.abs(g1-g2) > bestDif){
+					bestDif = Math.abs(g1-g2);
+					bestInd = j;
+				}
+			}
+			//System.out.println("bestInd "+i+": "+bestInd);
+			if(g1 < g2){
+				son1.sonsPos[son1.numRectangles] = node.sonsPos[bestInd];
+				son1.putRectangle(node.rectangles[bestInd]);
+			}
+			else if(g2 < g1){
+				son2.sonsPos[son2.numRectangles] = node.sonsPos[bestInd];
+				son2.putRectangle(node.rectangles[bestInd]);
+			}
+			else if(son1.getMBR().getArea() < son2.getMBR().getArea()){
+				son1.sonsPos[son1.numRectangles] = node.sonsPos[bestInd];
+				son1.putRectangle(node.rectangles[bestInd]);
+			}
+			else if(son1.getMBR().getArea() < son2.getMBR().getArea()){
+				son2.sonsPos[son2.numRectangles] = node.sonsPos[bestInd];
+				son2.putRectangle(node.rectangles[bestInd]);
+			}
+			else if(son1.numRectangles < son2.numRectangles){
+				son1.sonsPos[son1.numRectangles] = node.sonsPos[bestInd];
+				son1.putRectangle(node.rectangles[bestInd]);
 			}
 			else{
-				Node aux = this.nodeToRAM(node.fatherPos);
-				for(int i = 0; i < aux.numRectangles; i++){
-					if(aux.sonsPos[i] == node.pos){
-						son1.pos = aux.sonsPos[i];
-						this.nodeToExt(son1); //guardar nodos
+				son2.sonsPos[son2.numRectangles] = node.sonsPos[bestInd];
+				son2.putRectangle(node.rectangles[bestInd]);
+			}
+			node.rectangles[bestInd] = null;
+			bestDif = -1;
+		}
+		if(node.isLeaf == 0){ /* Si node no era hoja, sus fragmentos tampoco */
+			son1.isLeaf = 0;
+			son2.isLeaf = 0;
+		}
+		if(node.fatherPos == -1){ /* Si node es la raiz */
+			Node newRoot = new Node(node.pos,0);
+			newRoot.fatherPos = node.fatherPos;
+			newRoot.putRectangle(son1.getMBR());
+			newRoot.putRectangle(son2.getMBR());
+			son1.fatherPos = newRoot.pos;
+			son2.fatherPos = newRoot.pos;
+			newRoot.isLeaf = 0;
+			newRoot.sonsPos[0] = son1.pos;
+			newRoot.sonsPos[1] = son2.pos;
+			this.nodeToExt(newRoot);
+			this.nodeToExt(son1); //guardar nodos
+			this.nodeToExt(son2);
+		}
+		else{ /* Si node no es la raiz */
+			Node father = this.nodeToRAM(node.fatherPos);
+			for(int i = 0; i < father.numRectangles; i++){
+				if(father.sonsPos[i] == node.pos){ /* Si se encuentra posición de node en su padre */
+					son1.pos = father.sonsPos[i]; /* son1 adopta la ex dirección de node en father */
+					son2.pos = nextDir-4*B; /* Ajuste de direcciones en disco para son2 */
+					nextDir = nextDir+2*B;
+					son1.fatherPos = father.pos; /* Se asigna father como padre en son1 */
+					father.rectangles[i] = son1.getMBR(); /* Se reemplaza en father el MBR de node por el de son1 */
+					if(father.numRectangles < 2*t){ /* Si el padre de node tiene espacio suficiente */
+						son2.fatherPos = father.pos; /* Se asigna father como padre en son2 */
+						father.sonsPos[father.numRectangles] = son2.pos; /* Se asigna son2 como hijo en father */
+						father.putRectangle(son2.getMBR()); /* Se agrega el MBR de son2 en father */
+						this.nodeToExt(father);
+						this.nodeToExt(son1); /* Se guardan los nodos en disco */
 						this.nodeToExt(son2);
-						aux.putRectangle(son1.getMBR());
-						if(aux.numRectangles < 2*t)
-							aux.putRectangle(son2.getMBR());
-						this.QuadraticSplit(son2.getMBR(), aux);
-						break;
 					}
+					else{
+						Node recnode = this.QuadraticSplit(son2.getMBR(), father);
+						son2.fatherPos = recnode.pos; /* Se asigna father como padre en son2 */
+						recnode.sonsPos[recnode.numRectangles] = son2.pos; /* Se asigna son2 como hijo en father */
+						recnode.putRectangle(son2.getMBR()); /* Se agrega el MBR de son2 en father */
+						this.nodeToExt(recnode);
+						this.nodeToExt(son1); /* Se guardan los nodos en disco */
+						this.nodeToExt(son2);
+					}
+					break;
 				}
 			}
 		}
-		else
-			node.putRectangle(r);
+		return son2;
 	}
 	public int numRandom(int min, int max){
 		return ((int)(Math.random()*max-min+1));
@@ -269,7 +299,8 @@ public class RTree extends NodeSize{
 		tree.nodeToExt(node);
 		int x1,y1,w,h;
 		Rectangle r;
-		for(int i = 0; i < 2*tree.t+1; i++){
+		/* GENERADORA DE RECTÁNGULOS PARA INSERTAR */
+		for(int i = 0; i < 1000*tree.t; i++){
 			x1 = tree.numRandom(0, 90);
 			y1 = tree.numRandom(0, 90);
 			w = tree.numRandom(0, 10);
@@ -277,13 +308,13 @@ public class RTree extends NodeSize{
 			r = new Rectangle(new Vertex(x1,y1),w,h);
 			tree.insertar(r, 0);
 		}
-		tree.buscar(new Rectangle(new Vertex(0,0),100,100),0);
-		node = tree.nodeToRAM(8192);
+		tree.buscar(new Rectangle(new Vertex(30,30),10,10),0);
+		node = tree.nodeToRAM(0*tree.B);
 		for(int i = 0; i < node.numRectangles; i++)
-			System.out.println("Rectangle "+i+": "+node.rectangles[i].toString());
+			System.out.println("Rectangle "+i+": "+node.sonsPos[i]);
 		System.out.println("buscarResultSize: "+tree.buscarResult.size());
-		for(int i = 0; i < tree.buscarResult.size(); i++)
-			System.out.println("buscarResult "+i+": "+tree.buscarResult.get(i).toString());
+		/*for(int i = 0; i < tree.buscarResult.size(); i++)
+			System.out.println("buscarResult "+i+": "+tree.buscarResult.get(i).toString());*/
 		System.out.println(node.sonsPos[0]);
 	}
 }
